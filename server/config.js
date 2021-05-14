33e30a7328ec4deded78571da7f7a6ab6872aa71
Justin Pierce Tucker <57789046+justin1732@users.noreply.github.com>
2021-05-14 16:00:56 -0500

    Spacing out CORS

--- server/config.js
+++ server/config.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
-const config = {
-    shipengine: {
-        apiKey: process.env.SHIPENGINE_SANDBOX_API_KEY
-      },
-port: process.env.PORT || 8000,
+// const config = {
+//     shipengine: {
+//         apiKey: process.env.SHIPENGINE_SANDBOX_API_KEY
+//       },
+// port: process.env.PORT || 8000,
 
 
-};
+// };
 
-config.shippenguin.url = isProd ? process.env.SHIPPENGUIN_URL : `http://localhost:${config.port}`;
+// config.shippenguin.url = isProd ? process.env.SHIPPENGUIN_URL : `http://localhost:${config.port}`;
 
-module.exports = config;
+// module.exports = config;
