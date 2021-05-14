ae1a4d61bcebeed20b58de606ddf3f5ef86cbd53
justin1732 <57789046+justin1732@users.noreply.github.com>
2021-05-14 01:37:09 -0500

    Updating Address

--- src/AddressInput.js
+++ src/AddressInput.js
@@ -16,7 +16,6 @@ class Addressinput extends Component {
 
             <div className="card"><div className="card-body">
             <AddressItem label="o Street Address" id="street" value={this.props.street} onChange={this.handleChange} placeholder=""/>
-            <AddressItem label="o Street Address 2" id="street" value={this.props.street} onChange={this.handleChange} placeholder=""/>
             <AddressItem label="o City" id="city" value={this.props.city} onChange={this.handleChange} placeholder=""/>    
             <AddressItem label="o State (two letters)" id="state" value={this.props.state} onChange={this.handleChange} placeholder=""/>   
             <AddressItem label="o Zip Code" id="zipcode" value={this.props.zipcode} onChange={this.handleChange} placeholder=""/> 
